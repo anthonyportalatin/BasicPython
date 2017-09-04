ba7da806f1229ecb49f34f0e26dd3131816bf5ae
Anthony Portalatin <anthony.portalatin@upr.edu>
2017-09-04 18:43:01 -0400

    Functions
    This proram shows the use of functions and how to call them.

---
+++ Functions.py
@@ -0,0 +1,12 @@
+#This program shows the basic use of functions
+
+raw = input("Type the word you want to see: ")
+print (raw)
+
+#Function
+def receive():
+    this = input("Type the word you want to see: ")
+    return this
+
+#Calling Function
+print (receive())
