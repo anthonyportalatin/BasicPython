c969fbf03898eb24eadd921a700a6d6719fee7ad
Anthony Portalatin <anthony.portalatin@upr.edu>
2017-09-04 00:58:53 -0400

    If, Else
    Sample program to show the use of if and else.

---
+++ IfSample.py
@@ -0,0 +1,11 @@
+#This is a sample program
+
+print("Welcome to the Amusment Room")
+print("Choose your Room: Press 1 or 2")
+
+choice = input("Type 1 or 2, and hit Enter")
+
+if (choice==1):
+    print("This is the RPG Room")
+else:
+    print("This is the 3D Room")
