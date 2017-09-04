17ad818ef2ba0125c2fb34f0afb1258ddc80d33e
Anthony Portalatin <anthony.portalatin@upr.edu>
2017-09-04 18:43:25 -0400

    Loop
    Shows loop usage and implementations.

---
+++ LoopExample.py
@@ -0,0 +1,30 @@
+#This are loop examples
+
+target = 0
+while (target < 10):
+    target = target + 1
+    print(target)
+
+ei = 7
+while (ei != 0):
+    print(ei)
+    ei = ei - 1
+
+print("ei now equals ", ei)
+print("This ends everything")
+
+this_list = [4, 1 , 5, 3, 7]
+for content in this_list:
+    print (content)
+
+print ("The End.")
+
+chant = input("Who do you cheer for?")
+
+for character in chant:
+    chanting = "Gimme " + character + "!"
+    print(chanting)
+    print(character + "!")
+
+print("What does it say?")
+print(chant)
