5d136b4447d4fb095a201dc2c1dcff360a2f8853
Anthony Portalatin <anthony.portalatin@upr.edu>
2017-09-04 00:59:52 -0400

    Luck Game
    Shows the implementation of if, else, and elif with a luck based game.

---
+++ LuckyOne.py
@@ -0,0 +1,15 @@
+#This is a practice program to test luck
+
+print("Enter a, b, c, or d, and test your luck.")
+answer = input("Your answer: ")
+
+if (answer=="a"):
+    print("You will meet a beautiful stranger!")
+elif (answer=="b"):
+    print("You will meet a fateful encounter, beware...")
+elif (answer=="c"):
+    print("You will get a reward from your mom!")
+elif (answer=="d"):
+    print("Today your life will completely change!")
+else:
+    print("You will be unlucky for the rest of your life!")
